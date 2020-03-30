afcdaf8692c36e0978c1ec7fb85b5049c6beaafe
JasonHaque <zahinjason220434@gmail.com>
2020-03-30 22:10:19 +0600

    added new ib outlet and action function

--- Sweet Tooth/ViewController.swift
+++ Sweet Tooth/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getStartedPushed(_ sender: Any) {
+    }
+    
 }
 
