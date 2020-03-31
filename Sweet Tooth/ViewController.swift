5b219425f714c85f64382d259f3c4beeecd80279
JasonHaque <zahinjason220434@gmail.com>
2020-03-31 19:28:58 +0600

    added new code

--- Sweet Tooth/ViewController.swift
+++ Sweet Tooth/ViewController.swift
@@ -4,16 +4,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var welcomeLabel: UILabel!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        welcomeLabel.text = ""
+        
     }
 
     @IBAction func getStartedPushed(_ sender: Any) {
-        welcomeLabel.text = "Let's begin this epic journey now"
-        let navigationView = storyboard?.instantiateViewController(identifier: "QuizView") as? QuizViewController
-        view.window?.rootViewController = navigationView
-        view.window?.makeKeyAndVisible()
+        
+        
+        let textArray = ["welcome to sweet tooth","here we will ask you about sweet snacks","And you will get a surprise","Lets get started"]
+        (sender as AnyObject).setTitle("Next",for: .normal)
+        if(count < textArray.count){
+            welcomeLabel.text = textArray[count]
+            count += 1
+        }
+        else{
+            (sender as AnyObject).setTitle("On to the next page",for: .normal)
+            
+            let navigationView = storyboard?.instantiateViewController(identifier: "QuizView") as? QuizViewController
+            view.window?.rootViewController = navigationView
+            view.window?.makeKeyAndVisible() 
+        }
+        
+        
     }
     
 }
